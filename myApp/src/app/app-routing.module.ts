76ac712133d11da5883a7d5937c66519edf46b9a
Tim <gauquelintimothe@gmail.com>
2019-10-29 12:56:33 +0100

    WIP

--- myApp/src/app/app-routing.module.ts
+++ myApp/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { SelectedcardComponent } from './tab1/pagesmasterduel/selectedcard/selectedcard.component';
+import { BanlistComponent } from './tab1/pagesmasterduel/banlist/banlist.component';
+import { MycardlistComponent } from './tab1/pagesmasterduel/mycardlist/mycardlist.component';
+import { DecklistComponent } from './tab1/pagesmasterduel/decklist/decklist.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+  {path: 'duelmasterdecklist', component: DecklistComponent},
+  {path: 'duelmastercardlist', component: MycardlistComponent},
+  {path: 'duelmasterbanlist', component: BanlistComponent},
+  {path: 'duelmastercard/:id', component: SelectedcardComponent},
+  {path: '', loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)},
 ];
 @NgModule({
   imports: [
--- myApp/src/app/app.module.ts
+++ myApp/src/app/app.module.ts
@@ -8,11 +8,27 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DecklistComponent } from './tab1/pagesmasterduel/decklist/decklist.component';
+import { MycardlistComponent } from './tab1/pagesmasterduel/mycardlist/mycardlist.component';
+import { BanlistComponent } from './tab1/pagesmasterduel/banlist/banlist.component';
+import { SelectedcardComponent } from './tab1/pagesmasterduel/selectedcard/selectedcard.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+  AppComponent,
+  DecklistComponent,
+  MycardlistComponent,
+  BanlistComponent,
+  SelectedcardComponent
+],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    HttpClientModule,
+  ],
   providers: [
     StatusBar,
     SplashScreen,
